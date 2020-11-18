083dffa88b4f3540a8cfe7f0485c912f28e2f225
Andreas Müller <schnitzeltony@gmail.com>
2020-11-18 19:58:43 +0100

    Add RPC_GetDriveInfo to get rid of Unix like mount displays
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/vf_files.cpp
+++ src/vf_files.cpp
@@ -1,6 +1,7 @@
 #include "vf_files.h"
 #include "dirwatcherentry.h"
 #include "defaultpathentry.h"
+#include <QStorageInfo>
 
 using namespace vfFiles;
 
@@ -18,6 +19,13 @@ bool vf_files::initOnce()
         m_entity->initModule();
         m_entity->createComponent("EntityName", "_Files", true);
         m_entity->createRpc(this, "RPC_CopyFile", VfCpp::cVeinModuleRpc::Param({{"p_source", "QString"},{"p_dest", "QString"},{"p_overwrite", "bool"}}));
+        m_entity->createRpc(this, "RPC_GetDriveInfo",
+                            VfCpp::cVeinModuleRpc::Param({
+                                                             {"p_mountDir", "QString"},
+                                                             {"p_localeName", "QString"},
+                                                             {"p_getDriveName", "bool"},
+                                                             {"p_getMemTotal", "bool"},
+                                                             {"p_getMemAvail", "bool"}}));
     }
     return true;
 }
@@ -74,6 +82,49 @@ QVariant vf_files::RPC_CopyFile(QVariantMap p_params)
         qWarning("%s", qPrintable(strError));
     }
     return ok;
+}
+
+QVariant vf_files::RPC_GetDriveInfo(QVariantMap p_params)
+{
+    QString strError;
+
+    QString mountDir = p_params["p_mountDir"].toString();
+    QString localeName = p_params["p_localeName"].toString();
+    bool getDriveName = p_params["p_getDriveName"].toBool();
+    bool getMemTotal = p_params["p_getMemTotal"].toBool();
+    bool getMemAvail = p_params["p_getMemAvail"].toBool();
+
+    bool mountExists = QFile::exists(mountDir);
+    // check some obvious plausis first
+    if(!mountExists) {
+        appendErrorMsg(strError, mountDir + QStringLiteral(" does not exist"));
+    }
+
+    QStringList listMountInfo;
+    if(strError.isEmpty()) {
+        QStorageInfo storageInfo(mountDir);
+        if(storageInfo.isValid()) {
+            if(getDriveName) {
+                listMountInfo.append(QStringLiteral("name:") + storageInfo.name());
+            }
+            if(getMemTotal || getMemAvail) {
+                QLocale locale(localeName);
+                qint64 bytes;
+                if(getMemTotal) {
+                    bytes = storageInfo.bytesTotal();
+                    listMountInfo.append(QStringLiteral("total:") + locale.formattedDataSize(bytes));
+                }
+                if(getMemAvail) {
+                    bytes = storageInfo.bytesAvailable();
+                    listMountInfo.append(QStringLiteral("avail:") + locale.formattedDataSize(bytes));
+                }
+            }
+        }
+        else {
+            appendErrorMsg(strError, QStringLiteral("Could not create storage info for ") + mountDir);
+        }
+    }
+    return listMountInfo;
 }
 
 bool vf_files::addDirToWatch(const QString dir,
