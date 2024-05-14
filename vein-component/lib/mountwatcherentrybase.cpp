5eb3e6dbc6c6b67f27bd72d2746c11fe701f072b
Andreas Müller <schnitzeltony@gmail.com>
2024-05-14 22:51:33 +0200

    MountWatcherEntryBase: Close thread properly / avoid double creation
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- vein-component/lib/mountwatcherentrybase.cpp
+++ vein-component/lib/mountwatcherentrybase.cpp
@@ -1,7 +1,6 @@
 #include "mountwatcherentrybase.h"
 #include "mountwatcherworker.h"
 #include <QDir>
-#include <QThread>
 
 using namespace vfFiles;
 
@@ -12,10 +11,18 @@ MountWatcherEntryBase::MountWatcherEntryBase(QObject *parent) :
 {
 }
 
+MountWatcherEntryBase::~MountWatcherEntryBase()
+{
+    m_thread.quit();
+    m_thread.wait();
+}
+
 bool MountWatcherEntryBase::create(const QString procFileMount, const QString mountBasePath)
 {
     // Plausis first
     QString strErrorMsg;
+    if(m_created)
+        appendErrorMsg(strErrorMsg, QStringLiteral("MountWatcherEntry::create cannot be called more thanm once"));
     QFile tmpFile(procFileMount);
     if(!tmpFile.exists())
         appendErrorMsg(strErrorMsg, QStringLiteral("MountWatcherEntry::create: Could not find proc-file: ") + procFileMount);
@@ -25,19 +32,18 @@ bool MountWatcherEntryBase::create(const QString procFileMount, const QString mo
 
     bool ok = strErrorMsg.isEmpty();
     if(ok) {
+        m_created = true;
         // prepare polling thread
-        QThread *thread = new QThread();
         MountWatcherWorker *worker = new MountWatcherWorker(procFileMount, mountBasePath);
-        worker->moveToThread(thread);
+        worker->moveToThread(&m_thread);
 
         // thread start(/stop)
-        QObject::connect(thread, &QThread::started, worker, &MountWatcherWorker::startPoll, Qt::QueuedConnection);
-        QObject::connect(thread, &QThread::finished, worker, &MountWatcherWorker::deleteLater);
+        QObject::connect(&m_thread, &QThread::started, worker, &MountWatcherWorker::startPoll, Qt::QueuedConnection);
 
         // mount changes
         QObject::connect(worker, &MountWatcherWorker::mountChanged, this, &MountWatcherEntryBase::sigMountsChanged, Qt::QueuedConnection);
 
-        thread->start();
+        m_thread.start();
     }
     else
         qWarning("%s", qPrintable(strErrorMsg)); // warnings created by << are quoted which is ugly
--- vein-component/lib/mountwatcherentrybase.h
+++ vein-component/lib/mountwatcherentrybase.h
@@ -2,6 +2,7 @@
 #define MOUNTWATCHERENTRYBASE_H
 
 #include <QObject>
+#include <QThread>
 
 namespace vfFiles {
 
@@ -10,10 +11,15 @@ class MountWatcherEntryBase : public QObject
     Q_OBJECT
 public:
     explicit MountWatcherEntryBase(QObject *parent = nullptr);
+    ~MountWatcherEntryBase();
     bool create(const QString procFileMount,
                 const QString mountBasePath);
 signals:
     void sigMountsChanged(const QStringList mounts);
+
+private:
+    bool m_created = false;
+    QThread m_thread;
 };
 
 }
